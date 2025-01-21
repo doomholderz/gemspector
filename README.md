f005e8c81f75e8a16e778249ed6942fa03e3eb7a
doomholderz <joeharris@gocardless.com>
2025-01-21 09:04:37 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gemspector
-CLI tool to identify malicious Gems distributed via RubyGems 
+CLI tool to identify malicious Gems distributed via RubyGems.
+
+This project focusses on using meta-heuristics of the package to identify either malicious packages, or maliciously taken-over packages.
+
+To scan a set of Gems defined in a Gemfile, run `scanner.py -d examples/Gemfile`
+
+Current capabilities:
+- Identifies if publicised maintainer emails are using expired domains
+
+Future capabilities:
+- Identifies if publicised maintainer emails are using compromised domains
+- Identifies repository integrity mismatch with source repository
+- Identifies potentially disposable/one-time-use emails for maintainers
+- Identifies unusual package updates (e.g. anomalies in time of day/update descriptions)
---
+++ scanner.py
@@ -0,0 +1,77 @@
+import re
+import requests
+import whois
+from datetime import datetime
+import argparse
+import os
+
+def check_domain_expiry(domain):
+    try:
+        w = whois.whois(domain)
+        if w.expiration_date:
+            expiration_date = w.expiration_date[0] if isinstance(w.expiration_date, list) else w.expiration_date
+
+            if expiration_date < datetime.now():
+                return f"The domain '{domain}' is expired."
+            else:
+                return f"The domain '{domain}' is valid until {expiration_date}"
+        else:
+            return f"No expiration date found for the domain '{domain}'. It may not be registered."
+    except Exception as e:
+        return f"Error checking domain '{domain}': {e}"
+
+def extract_email_domain(email):
+    pattern = r'(?<=@)[^.]+\.[^.]+'
+    
+    match = re.search(pattern, email)
+    
+    if match:
+        return match.group(0)
+    else:
+        return None
+
+def extract_gems(gemfile_path):
+    gems = set()
+    gem_regex = re.compile(r"^\s*gem\s+['\"]([^'\"]+)['\"]")
+
+    with open(gemfile_path, 'r') as file:
+        for line in file:
+            match = gem_regex.match(line)
+            if match:
+                gems.add(match.group(1))
+
+    return sorted(gems)
+
+def get_gem_owners(gem_name, output_format='json', base_url='https://rubygems.org'):
+    url = f"https://rubygems.org/api/v1/gems/{gem_name}/owners.json"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        return f"Error fetching data: {e}"
+
+def main():
+    parser = argparse.ArgumentParser(description="Scan a Gemfile for listed gems.")
+    parser.add_argument("-d", "--directory", required=True, help="Path to the Gemfile")
+    args = parser.parse_args()
+    gemfile_path = args.directory
+    if not os.path.exists(gemfile_path):
+        print(f"Error: The specified Gemfile does not exist: {gemfile_path}")
+        return
+
+    used_gems = extract_gems(gemfile_path)
+    for gem in used_gems:
+        gem_owners = get_gem_owners(gem)
+        if type(gem_owners) == list:
+            print(f"{gem} has {len(gem_owners)} maintainer(s).")
+            for owner in gem_owners:
+                if "email" in owner and owner["email"]:
+                    email_domain = extract_email_domain(owner["email"])
+                    print(f"\temail: {owner["email"]}")
+                    active_email_domain = check_domain_expiry(email_domain)
+                    print(f"\t{active_email_domain}")
+
+if __name__ == "__main__":
+    main()
